7d80ec9e55f5c3a6c92f4cdc500610dd11d87d43
EduardoTartas <eduardos.tartas@gmail.com>
2025-01-27 19:35:15 -0400

    ajuste caminho do csv

--- src/services/csvServices.ts
+++ src/services/csvServices.ts
@@ -4,7 +4,7 @@ import { clear } from '../utils/functions';
 import chalk from 'chalk';
 import { admRole, guestRole, profRole} from './roleServices';
 
-const usersFilePath = '/home/eduardotartas/Documents/gerencimento-de-usuarios/data/users.csv';
+const usersFilePath = '../data/users.csv';
 export let defaultUser: Users;
 export let users: Users[] = [];
 loadUsers();
